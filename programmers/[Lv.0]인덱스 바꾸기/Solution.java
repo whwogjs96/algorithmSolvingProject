c17bc0408621ffaeff491b24a977d240cffeb5ec
whwogjs96 <68106399+whwogjs96@users.noreply.github.com>
2022-11-20 20:59:56 +0900

    프로그래머스 인덱스 바꾸기
    프로그래머스 인덱스 바꾸기
    문제 풀이

---
+++ programmers/[Lv.0]인덱스 바꾸기/Solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        char[] charArray = my_string.toCharArray();
+        char char1 = charArray[num1];
+        charArray[num1] = charArray[num2];
+        charArray[num2] = char1;
+        return new String(charArray);
+    }
+}
