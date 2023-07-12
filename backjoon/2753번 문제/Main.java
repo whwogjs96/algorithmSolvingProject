2730b609fb36ef738cc00d10e942b8d0a3a39e2b
Jaeheon Cho <68106399+whwogjs96@users.noreply.github.com>
2023-07-12 12:07:16 +0900

    백준 2753번 문제
    백준 2753번 문제 윤년
    문제 풀이

---
+++ backjoon/2753번 문제/Main.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int year = Integer.parseInt(br.readLine());
+        if(year%4 ==0) {
+            if(year%100 != 0) {
+                System.out.println("1");
+            }
+            else if(year%400 == 0) System.out.println("1");
+            else System.out.println("0");
+        }
+        else {
+            System.out.println("0");
+        }
+    }
+}
