6f9b02c02f9fa018ce38284209557347d186d120
whwogjs96 <whwogjs96@gmail.com>
2024-01-22 00:24:59 +0900

    백준 25083번 문제
    백준 25083번
    문제 풀이

---
+++ backjoon/25083번 문제/main.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    println("         ,r'\"7")
+    println("r`-_   ,'  ,/")
+    println(" \\. \". L_r'")
+    println("   `~\\/")
+    println("      |")
+    println("      |")
+}
